3daab748d7bc879e7166022896acdf4346a8e721
Lysytsia <sasha4ox@gmail.com>
2026-03-11 01:45:48 +0200

    counties

--- src/components/CountrySelect/COUNTRIES.js
+++ src/components/CountrySelect/COUNTRIES.js
@@ -1,6 +1,6 @@
 /** ISO 3166-1 alpha-2 codes shown in registration and profile country select. */
 export const COUNTRIES = [
   'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IE', 'IT',
-  'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'UA',
+  'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'UA', 'RU', 'BY', 'KZ',
   'US', 'CA', 'GB', 'AU', 'NZ', 'JP', 'CN', 'IN', 'BR', 'MX', 'KR', 'SG', 'CH', 'NO', 'TR', 'IL', 'AE', 'SA', 'ZA',
 ];
